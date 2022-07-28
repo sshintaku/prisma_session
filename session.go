b7b43a93148d99c83b0f46ec487c33a0acede285
Seiji Shintaku <32934719+sshintaku@users.noreply.github.com>
2022-07-28 07:53:32 -0400

    Update session.go

--- session.go
+++ session.go
@@ -2,11 +2,14 @@ package prisma_session
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"regexp"
 	"strconv"
+	"syscall"
 
 	CloudType "github.com/sshintaku/cloud_types"
 	"github.com/sshintaku/web_requests"
@@ -19,7 +22,7 @@ type Session struct {
 	ApiUrl         string
 }
 
-func (s *Session) CreateSession(baseUrl string) {
+func (s *Session) CreateSession() {
 	username := os.Getenv("APIKEY")
 	if username == "" {
 		log.Fatalln("APIKEY environment variable is not set.")
@@ -28,8 +31,8 @@ func (s *Session) CreateSession(baseUrl string) {
 	if password == "" {
 		log.Fatalln("PASSWORD environment variable is not set.")
 	}
-	s.ApiUrl = baseUrl
-	authResponse, authError := web_requests.GetJWTToken(s.ApiUrl+"login", username, password)
+	url := s.ApiUrl + "login"
+	authResponse, authError := web_requests.GetJWTToken(url, username, password)
 	if authError != nil {
 		fmt.Println("Error with authorization session creation.")
 		log.Fatal(authError)
@@ -41,6 +44,19 @@ func (s *Session) CreateSession(baseUrl string) {
 	if baseUrlError != nil {
 		log.Fatal(baseUrlError)
 	}
+}
+
+func (s *Session) GetCompliancePosture(filter CloudType.CSPMFilter) {
+	url := s.ApiUrl + "compliance/posture"
+	json, error := json.Marshal(filter)
+	if error != nil {
+		log.Fatal(error)
+	}
+	result, resultError := web_requests.PostMethod(url, json, s.Token)
+	if resultError == nil {
+		log.Fatal(resultError)
+	}
+	fmt.Println(string(*result))
 }
 
 func Find(slice []string, val string) bool {
@@ -83,7 +99,7 @@ func (s *Session) GetSampleDeployedImages() []CloudType.ComplianceObject {
 	return complianceList
 }
 
-func (s *Session) GetSamepleContainers() []CloudType.ContainerInfo {
+func (s *Session) GetSampleContainers() []CloudType.ContainerInfo {
 
 	var jsonObject []CloudType.ContainerInfo
 	var complianceList []CloudType.ContainerInfo
@@ -146,6 +162,110 @@ func (s *Session) GetDeployedImages() []CloudType.ComplianceObject {
 	}
 
 	return complianceList
+}
+
+func (s *Session) GetRegistry(offset int, maxRecords int) []CloudType.RegistryScan {
+	var list []CloudType.RegistryScan
+	flag := true
+	offsetValue := offset
+
+	for flag {
+		uri := s.ComputeBaseUrl + "/api/v1/registry?limit=50&offset=" + strconv.Itoa(offsetValue)
+		//fmt.Println(strconv.Itoa(offsetValue))
+		//time.Sleep(1 * time.Second)
+		results, resultError := web_requests.GetMethod(uri, s.Token)
+
+		if errors.Is(resultError, syscall.ECONNRESET) {
+			retryResult, retryError := web_requests.GetMethod(uri, s.Token)
+			if retryError != nil {
+				log.Fatal(resultError)
+			}
+			results = retryResult
+
+		}
+		if string(results) == "null" || offsetValue >= maxRecords {
+			flag = false
+		} else {
+			var jsonObject []CloudType.RegistryScan
+			json.Unmarshal(results, &jsonObject)
+			offsetValue = offsetValue + 50
+			//fmt.Println(string(results))
+			list = append(list, jsonObject...)
+		}
+
+	}
+	return list
+}
+
+func (s *Session) GetCSPMAlerts(payload CloudType.AlertQuery) ([]CloudType.AlertModel, error) {
+	uri := s.ApiUrl + "v2/alert"
+	request, requestError := http.NewRequest("GET", uri, nil)
+	if requestError != nil {
+		log.Fatalln(requestError)
+	}
+	q := request.URL.Query()
+	q.Add("timeType", payload.TimeType)
+	q.Add("timeAmount", payload.TimeAmount)
+	q.Add("detailed", strconv.FormatBool(payload.Detailed))
+	q.Add("timeUnit", payload.TimeUnit)
+	q.Add("limit", "50")
+	q.Add("policy.severity", payload.Severity)
+	q.Add("policy.name", payload.PolicyName)
+	q.Add("alert.status", payload.AlertStatus)
+	switch {
+	case payload.ServiceName != nil:
+		q.Add("cloud.service", *payload.ServiceName)
+	}
+	request.Header.Add("x-redlock-auth", s.Token)
+	request.URL.RawQuery = q.Encode()
+	var result []CloudType.AlertModel
+
+	flag := true
+	for flag {
+		retryResult, retryError := web_requests.ProcessWebRequest(request)
+		if retryError != nil {
+			log.Fatalln(retryError)
+
+		}
+
+		var alert CloudType.AlertResponse
+		unmarshallError := json.Unmarshal(retryResult, &alert)
+		if alert.NextPageToken == "" || alert.NextPageToken == "null" {
+			flag = false
+		} else {
+			for _, item := range alert.AlertModelArray {
+				result = append(result, item)
+			}
+			if unmarshallError != nil {
+				log.Fatalln(unmarshallError)
+				return nil, unmarshallError
+			}
+			request.Header.Set("pageToken", alert.NextPageToken)
+		}
+	}
+	return result, nil
+}
+func (s *Session) GetRuntimeAudits(excludeAlarms []string) []CloudType.AuditType {
+	var list []CloudType.AuditType
+	flag := true
+	offsetValue := 0
+	for flag {
+		uri := s.ComputeBaseUrl + "/api/v1/audits/incidents?limit=50&offset=" + strconv.Itoa(offsetValue)
+		results, resultError := web_requests.GetMethod(uri, s.Token)
+		if resultError != nil {
+			log.Fatal(resultError)
+		}
+		if string(results) == "null" || string(results) == "" {
+			flag = false
+		} else {
+			var jsonObject []CloudType.AuditType
+			json.Unmarshal(results, &jsonObject)
+			offsetValue = offsetValue + 50
+			list = append(list, jsonObject...)
+		}
+
+	}
+	return list
 }
 
 func (s *Session) GetImageCVEInfo(cve string) []CloudType.ImageInfo {
