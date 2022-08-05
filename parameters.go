2725fe95fba164d886520ca4507d7a7bf1d85834
Seiji Shintaku <32934719+sshintaku@users.noreply.github.com>
2022-08-05 16:25:46 -0400

    Update parameters.go

--- parameters.go
+++ parameters.go
@@ -1,5 +1,13 @@
 package prisma_session
 
+import (
+	"encoding/json"
+	"log"
+	"os"
+
+	CloudType "github.com/sshintaku/cloud_types"
+)
+
 type Parameters struct {
 	AlarmLevels          []string
 	RegEx                string
@@ -7,4 +15,17 @@ type Parameters struct {
 	ApiUrl               string
 	IgnoreAudits         []string
 	AlertQueryParameters CloudType.AlertQuery
+}
+
+func ReadParameters() Parameters {
+	var params Parameters
+	paramBytes, err := os.ReadFile(".parameter.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	json.Unmarshal(paramBytes, &params)
+	if len(params.AlarmLevels) == 0 {
+		log.Fatalln("The alarm levels must not be empty.  There should be at least one value such as \"critical\"")
+	}
+	return params
 }
