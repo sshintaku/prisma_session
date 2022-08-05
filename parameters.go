b1401d43f65b14452bcbb0a361ff0b76c1acde4d
Seiji Shintaku <32934719+sshintaku@users.noreply.github.com>
2022-08-05 16:15:09 -0400

    Create parameters.go

---
+++ parameters.go
@@ -0,0 +1,8 @@
+type Parameters struct {
+	AlarmLevels          []string
+	RegEx                string
+	FixDate              int
+	ApiUrl               string
+	IgnoreAudits         []string
+	AlertQueryParameters CloudType.AlertQuery
+}
