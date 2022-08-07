239938f8a1896a96b46c9082c476bce643f735e4
Seiji Shintaku <32934719+sshintaku@users.noreply.github.com>
2022-08-06 22:27:12 -0400

    Update cwp.go

--- cwp.go
+++ cwp.go
@@ -1,6 +1,8 @@
 package prisma_session
 
 import (
+	"fmt"
+
 	CloudType "github.com/sshintaku/cloud_types"
 )
 
@@ -25,4 +27,44 @@ func inList(list []string, collectionName string) bool {
 		}
 	}
 	return false
+}
+
+func GetDataByCollection(collectionname string, data []CloudType.ComplianceObject) {
+	var list []CloudType.ComplianceObject
+	for _, item := range data {
+		result := isInCollection(collectionname, item.Collections)
+		if result {
+			list = append(list, item)
+		}
+	}
+
+	var critical, high, important, medium, low int
+	for _, item := range list {
+
+		for _, vulnerability := range item.VulnerabilityIssues {
+			switch vulnerability.Severity {
+			case "critical":
+				critical++
+			case "high":
+				high++
+			case "important":
+				important++
+			case "medium":
+				medium++
+			case "low":
+				low++
+			}
+		}
+
+	}
+	fmt.Println("For collection: " + collectionname)
+}
+
+func isInCollection(collectionName string, collectionArray []string) bool {
+	for _, item := range collectionArray {
+		if item == collectionName {
+			return true
+		}
+	}
+	return true
 }
